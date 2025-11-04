f460eae62109470927d2ee48bb16c0718ec59433
naban2910 <naban2910@oslomet.no>
2025-11-04 10:46:07 +0100

    test

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d83c2498c8cd4d4df636b90657ced6f180a997c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96b384b0a290b7ad152aa03ec3428c8083981882")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
