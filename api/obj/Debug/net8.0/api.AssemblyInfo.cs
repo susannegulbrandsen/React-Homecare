0135ddb4abd4401af5a956d7edaa1f60157c92fd
vildenerem <viner0602@oslomet.no>
2025-11-27 12:36:32 +0100

    Fikset Unit Testing:)

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d9a32a3c1ecf0963139968fc49ff1d873d40213")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcbc166084d3cb82664c706a63a8eda7404462b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
