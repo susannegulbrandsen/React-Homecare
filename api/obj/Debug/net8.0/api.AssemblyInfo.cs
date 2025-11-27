ca9e0066e37ee2ab19e603787dc228bc2f5ffc54
susannegulbrandsen <143507850+susannegulbrandsen@users.noreply.github.com>
2025-11-27 13:36:39 +0100

    build fail

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeae4df4d433a4bad80d5322e26c8bd24c4aafe3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f7aa437703040a164ef2d1d560230db8d307ab3")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
